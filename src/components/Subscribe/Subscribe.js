70ef86a91dfe40521c3f7cebbbfb097404baea5c
John "Fitzy" DeLancey <fitzy@fct.dev>
2020-08-25 14:05:36 -0500

    Subscription component added

---
+++ src/components/Subscribe/Subscribe.js
@@ -0,0 +1,12 @@
+// @flow strict
+import React from 'react';
+import styles from './Subscribe.module.scss';
+
+
+const Author = () => (
+  <div className={styles['subscribe']}>
+    <script async data-uid="98430ec254" src="https://skilled-maker-9131.ck.page/98430ec254/index.js"></script>
+  </div>
+);
+
+export default Author;
