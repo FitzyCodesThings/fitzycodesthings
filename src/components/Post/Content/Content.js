3a79c68d7927737170b3b40fec58f339333efa73
John "Fitzy" DeLancey <fitzy@fct.dev>
2020-09-03 12:49:23 -0500

    azure functions post

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -10,7 +10,7 @@ type Props = {
 
 const Content = ({ body, title, socialImage }: Props) => (
   <div className={styles['content']}>
-    {(typeof socialImage === 'string' && socialImage !== '') ?? <img src={socialImage} />}
+    {(typeof socialImage === 'string' && socialImage.trim() !== '') ? <img src={socialImage} /> : null}
     <h1 className={styles['content__title']}>{title}</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
