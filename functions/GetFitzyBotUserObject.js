14ca1afe97cd66773917c9d16bd21d014172824f
John "Fitzy" DeLancey <fitzy@fct.dev>
2020-09-19 16:01:01 -0500

    lint fix

--- functions/GetFitzyBotUserObject.js
+++ functions/GetFitzyBotUserObject.js
@@ -1,27 +1,26 @@
-const dcsdk = require("dragonchain-sdk");
+const dcsdk = require('dragonchain-sdk');
 
 // functions/GetFitzyBotUserObject.js
 exports.handler = async (event) => {
-    const commandSource = event.queryStringParameters.commandSource;
-    const targetUsername = event.queryStringParameters.targetUsername;
-    const contractId = event.queryStringParameters.contractId;
+  const { commandSource } = event.queryStringParameters;
+  const { targetUsername } = event.queryStringParameters;
+  const { contractId } = event.queryStringParameters;
 
-    if (typeof commandSource === "null" || commandSource.trim() == "" || 
-        typeof targetUsername === "null" || targetUsername.trim() == "")
-    {
-        return {
-            statusCode: 500,
-            body: 'Invalid parameters'
-        };
-    } else {
-
-        const client = await dcsdk.createClient();
-
-        const key = `${commandSource}-${targetUsername}`;
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(await client.getSmartContractObject({"key": key, "smartContractId": contractId}))
-        };
-    }
+  if (typeof commandSource === 'null' || commandSource.trim() == ''
+        || typeof targetUsername === 'null' || targetUsername.trim() == ''
+        || typeof contractId === 'null' || contractId.trim() == '') {
+    return {
+      statusCode: 500,
+      body: 'Invalid parameters'
+    };
   }
+
+  const client = await dcsdk.createClient();
+
+  const key = `${commandSource}-${targetUsername}`;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(await client.getSmartContractObject({ key, smartContractId: contractId }))
+  };
+};
