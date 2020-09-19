cdde5d006b2d2bc26fd64d00efe70470317f00da
John "Fitzy" DeLancey <fitzy@fct.dev>
2020-09-19 13:40:06 -0500

    Function update

--- functions/GetFitzyBotUserObject.js
+++ functions/GetFitzyBotUserObject.js
@@ -1,8 +1,24 @@
+const dcsdk = require("dragonchain-sdk");
+
 // functions/GetFitzyBotUserObject.js
 exports.handler = async (event) => {
-    const subject = event.queryStringParameters.name || 'World';
-    return {
-      statusCode: 200,
-      body: `Hello ${subject}!`,
+    const commandSource = event.queryStringParameters.commandSource;
+    const targetUsername = event.queryStringParameters.targetUsername;
+
+    if (typeof commandSource === null || commandSource.trim() == "" || 
+        typeof targetUsername === null || targetUsername.trim() == "")
+    {
+        return {
+            statusCode: 500,
+            body: 'Invalid parameters'
+        };
+    } else {
+
+        const client = await dcsdk.createClient();
+
+        return {
+            statusCode: 200,
+            body: await client.getStatus()
+        };
     }
   }
